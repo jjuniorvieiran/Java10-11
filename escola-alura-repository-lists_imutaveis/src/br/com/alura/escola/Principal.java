a8542eea44e7d1d058099c7d8e500cd47d7370e7
Junior <junior.jaava@gmail.com>
2019-10-18 22:43:37 +0200

    Using Collectors.groupingBy. Java9 way create lists and dates.

--- escola-alura-repository-lists_imutaveis/src/br/com/alura/escola/Principal.java
+++ escola-alura-repository-lists_imutaveis/src/br/com/alura/escola/Principal.java
@@ -1,16 +1,22 @@
 package br.com.alura.escola;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import br.com.alura.escola.modelo.Curso;
+import br.com.alura.escola.modelo.Turma;
 import br.com.alura.escola.servico.AlunoServico;
+import br.com.alura.escola.servico.TurmaServico;
 
 public class Principal {
 
 	public static void main(String...strings) {
 
 		AlunoServico alunoServico = new AlunoServico();
+		TurmaServico turmaServico = new TurmaServico();
 		//alunoServico.listar().stream().forEach(System.out::println);
 		
 		List<String> alunos = alunoServico.listar().stream()
@@ -18,7 +24,16 @@ public class Principal {
 				.map(aluno -> aluno.toUpperCase())
 				.collect(Collectors.toList());
 
-		System.out.println(alunos);
 
+		Map<Curso, List<Turma>> turmasPorCurso = turmaServico.listar().stream()
+				.filter(a -> LocalDate.of(2019, 4,3).equals(a.getInicio()))
+				.collect(Collectors.groupingBy(Turma::getCurso));
+
+		Map<Curso, List<Turma>> turmasPorCurso2 = turmaServico.listar().stream()
+				.collect(Collectors.groupingBy(Turma::getCurso,
+						Collectors.filtering(a -> LocalDate.of(2019, 12,3).equals(a.getInicio()), Collectors.toList()) ) );
+
+
+		System.out.println(turmasPorCurso2);
 	}
 }
